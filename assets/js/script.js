bd59f183ec00a9c6c47732f2dc1dc267a7a9d29d
AdrianoA <adriano.armendaris@gmail.com>
2021-07-23 01:55:40 -0500

    hide start button on js file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Start button functionality
+let startButton=document.getElementById="start-button";
+let startSection=document.querySelector(".start-section");
+
+document.getElementById("start-button").addEventListener("click", function() {
+    startSection.style.visibility = "hidden";
+  });
