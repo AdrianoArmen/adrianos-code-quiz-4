f657198a78c7b38f382f03ee640d6dd982ea4329
AdrianoA <adriano.armendaris@gmail.com>
2021-07-24 01:07:00 -0500

    selectors added to main section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,43 @@
-// Start button functionality
-let startButton=document.getElementById="start-button";
-let startSection=document.querySelector(".start-section");
+// questions will be displayed on array object
+const questions = [
+    {
+        id: '1',
+        question: 'Quest1',
+    },
+    {
+        id: '2',
+        question: 'Quest2',
+    },
+    {
+        id: '3',
+        question: 'Quest3',
+    },
+]
+const choices = [
+    {
+        id: '1',
+        answer: 'Choice1',
+        questionid: '1',
+    },
+    {
+        id: '2',
+        answer: 'Choice2',
+        questionid: '1',
+    },
+    {
+        id: '3',
+        answer: 'Choice3',
+        questionid: '1',
+    },
+]
 
-document.getElementById("start-button").addEventListener("click", function() {
-    startSection.style.visibility = "hidden";
-  });
+let startSection = document.querySelector(".start-section");
+
+let quizSection = document.querySelector(".quiz-section");
+document.querySelector("#start-button").addEventListener("click", function () {
+    startSection.style.display = "none";
+    quizSection.style.display = "block";
+});
+
+
+let choicesSection=document.querySelector(".choices-section");
